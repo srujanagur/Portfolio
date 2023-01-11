3c35c3962a5350960cb261bc33543566ac0e8a6b
srujana <gurrala.srujana@outlook.com>
2023-01-11 15:39:48 +0100

    commit

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
+import "./Projects.css";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <div className="eachProject">
+        <h5>Recipe App</h5>
+        <p>Project Description</p>
+        <button className="projectButton">project link</button>
+        <button className="projectButton">live demo</button>
+      </div>
+      <div className="eachProject">
+        <h5>Recipe App</h5>
+        <p>Project Description</p>
+        <button className="projectButton">project link</button>
+        <button className="projectButton">live demo</button>
+      </div>
+      <div className="eachProject">
+        <h5>Recipe App</h5>
+        <p>Project Description</p>
+        <button className="projectButton">project link</button>
+        <button className="projectButton">live demo</button>
+      </div>
+      <div className="eachProject">
+        <h5>Recipe App</h5>
+        <p>Project Description</p>
+        <button className="projectButton">project link</button>
+        <button className="projectButton">live demo</button>
+      </div>
+    </div>
+  );
 }
